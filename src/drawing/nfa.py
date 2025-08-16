89ea545681221d9dcd087bfa937a3d47a687e2cf
PeDro0210 <pedroruben021@gmail.com>
2025-08-16 16:29:43 -0600

    quick fixed to rendering format

--- src/drawing/nfa.py
+++ src/drawing/nfa.py
@@ -1,11 +1,17 @@
 from graphviz import Digraph
 from parsing.nfa import NFA
 
-def add_state(nfa_render, visited, state, state_id):
+def add_state(nfa_render, visited, state, state_id, state_idx):
     if state not in visited:
         visited.add(state) # we add the state for not checking it again, in any case a klean operator does bad stuff
-        nfa_render.node(state_id, label=str()) # puttin the emtpy string for it to not have a label 
+
+        # cause it is an imperative way of doing it, the state will be filled from the right states to the left ones
+        if state.edge1 is None and state.edge2 is None:
+            nfa_render.node(state_id, label=str(state_idx), shape="doublecircle")  # the double circle for the notation that bidkar uses
+        else:
+            nfa_render.node(state_id, label=str(state_idx))# puttin the emtpy string for it to not have a label
         
+
         if state.edge1: # Checking for existance
             nfa_render.edge(state_id, str(id(state.edge1)), label=state.label if state.label else "E") # the one and only, friendly utf-8 epsilon
         if state.edge2: # Checking for existance
@@ -15,15 +21,24 @@ def add_state(nfa_render, visited, state, state_id):
 
 # given our NFA, retunrs a graph for graphviz to parse it
 def nfaToDiGraph(nfa: NFA):
+
     nfa_render:Digraph = Digraph() # where we render the things
+
     visited:set = set()
+
+    state_idx:int = 0 # having the count of the state
+
 
     # we push to the stack the initial point of the nfa, and we start pushing to the stack all the subsecuent ones
     stack = [(nfa.initial, str(id(nfa.initial)))]
 
+
     while stack:
         state, state_id = stack.pop()
-        add_state(nfa_render, visited,state, state_id)
+
+        state_idx += 1 
+        
+        add_state(nfa_render, visited,state, state_id, state_idx)
 
         # we put both edges (with there respective state obviosly) for being check
         if state.edge1 and state.edge1 not in visited:
