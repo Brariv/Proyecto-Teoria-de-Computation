9bcbc9bbe08191ff5d8a8e40155a6d2a1ccdc3f4
Brariv <riv23088@uvg.edu.gt>
2025-08-17 19:37:55 -0600

    Comments for the match

--- src/match/nfa_match.py
+++ src/match/nfa_match.py
@@ -1,25 +1,25 @@
+def followes(initial_state):
+    # All the states that can be reached and will be visited
+    states_to_visit = set()  
 
-# Helper function - Returns set of states that can be reached from state following e arrows
-def followes(state):
-    states = set()  # A set to store the reachable states
-    stack = [state]  # A stack to keep track of states to process
-    
+    # we push to the stack the initial point of the nfa, and we start pushing to the stack all the subsecuent ones
+    stack = [initial_state]  
+
     while stack:
-        current_state = stack.pop()  # Pop the last state from the stack
-        
-        # If the state is not already visited, process it
-        if current_state not in states:
-            states.add(current_state)
+        state = stack.pop()  
 
-            # If there is an 'edge1' and it's not None, push it onto the stack
-            if current_state.edge1 is not None:
-                stack.append(current_state.edge1)
-            
-            # If there is an 'edge2' and it's not None, push it onto the stack
-            if current_state.edge2 is not None:
-                stack.append(current_state.edge2)
+        # If the state is NOT visited, re-add it to the stack
+        if state not in states_to_visit:
+            states_to_visit.add(state)
 
-    return states
+            if state.edge1 is not None: # Checking for existance and has a transition different from epsilon
+                stack.append(state.edge1)
+
+            if state.edge2 is not None: # Checking for existance and has a transition different from epsilon
+                stack.append(state.edge2)
+
+    # Return all reachable states to visit
+    return states_to_visit
 
 def matchStringToNfa(nfa, string):
     current = set()  # The current set of states
@@ -29,21 +29,21 @@ def matchStringToNfa(nfa, string):
     current |= followes(nfa.initial)
 
     # Loop through each character in the string
+    # The main loop will process the tree for each character
     for s in string:
         
-        # Loop through the current set of states
         for c in current:
-            
-            # Check if state is labeled with the current character 's'
+
+            # Checks state's transition with the current character of the string
             if c.label == s:
-                
-                # Add reachable states to nexts
+
+                # we put the edge to be checked
                 nexts |= followes(c.edge1)
 
-        # Set current to next and clears out next
+        # Set current to next and starts over
         current = nexts
-        nexts = set()  # Reset nexts for the next iteration
+        nexts = set()  
 
-    # Check if the accept state is in the set of current states
+    # Check if the last state is an accept state
     return nfa.accept in current
 
