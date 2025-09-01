fe06104c57a0e4213a6c6189b7373df039fb432e
PeDro0210 <pedroruben021@gmail.com>
2025-09-01 15:14:13 -0600

    chore: added cli argument parsing

--- src/main.py
+++ src/main.py
@@ -1,33 +1,31 @@
 from drawing.nfa import nfaToDiGraph
 from match.nfa_match import matchStringToNfa
-from parsing.ast import astToGraph, postfixToAst
 from parsing.nfa import postfixToNfa
 from parsing.postfix import infixToPostfix
+from utils.argument_parsing import parseLexerArgs
 from utils.file_parsing import fileReader
 
-import sys
-sys.setrecursionlimit(10**8)
 
 if __name__ == "__main__":
-    filename:str = "files/regex.txt" 
-    lines:list[str] = fileReader(filename) 
 
-    string:str = input("Add your cool string :D :")
+    parse_arguments = parseLexerArgs();
 
-    print(lines)
+    lines:list[str] = fileReader(parse_arguments.regex_file); 
+
     for line in lines:
         postfix = infixToPostfix(line)
 
         nfa = postfixToNfa(postfix)
 
-        # for showing the Ast
-
-        if matchStringToNfa(nfa, string):
-            print("yes")
+        # For showing the nfa
+        if matchStringToNfa(nfa, parse_arguments.string):
+            print(f"The string: {parse_arguments.string}, is part of the regex : {line}")
         else:
-            print("no")
+            print(f"The string: {parse_arguments.string}, is not part of the regex : {line}")
 
         nfaToDiGraph(nfa).render(f"nfa_imgs/nfa_from_{line}", format="png", cleanup=True) # To lazy for having the count, just making a random tree
+
+        # For showing the dfa
 
 
 
--- src/utils/file_parsing.py
+++ src/utils/file_parsing.py
@@ -6,9 +6,5 @@ def fileReader(filename:str) -> list[str]:
             break
         except FileNotFoundError:
             print(f"File not found: {filename}")
-            filename = input("Enter the path to the text file: ")
-        except Exception as e:
-            print(f"Error reading {filename}: {e}")
-            filename = input("Enter the path to the text file: ")
     return [line.strip() for line in lines]
 
