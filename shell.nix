28bd7afd4206f202d0bfe939c59ec9b38a64bc8c
PeDro0210 <pedroruben021@gmail.com>
2025-09-13 12:47:30 -0600

    chore: refactoring flake for using pipy as JIT-Compiler

--- shell.nix
+++ shell.nix
@@ -1,11 +1,24 @@
 { pkgs, ... }:
+
+let
+  pythonPackages = pkgs.pypy3Packages;
+  types-networkx =
+    let
+      pname = "types-networkx";
+      version = "3.1.0.20231220";
+    in
+    pythonPackages.buildPythonPackage {
+      inherit pname version;
+      src = pkgs.fetchPypi {
+        inherit pname version;
+        sha256 = "sha256-ikAz9jGqoVEpob6MFNw6lj1/BL7Zev9kYJIBw5w0nN0=";
+      };
+      doCheck = false;
+    };
+in
 pkgs.mkShell {
-  packages = [
-    (pkgs.python3.withPackages (python-pkgs: [
-      python-pkgs.graphviz
-      python-pkgs.pprintpp
-      python-pkgs.setuptools
-    ]))
-    pkgs.pyright
+  buildInputs = with pythonPackages; [
+    graphviz
+    pprintpp
   ];
 }
