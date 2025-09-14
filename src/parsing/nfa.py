450f8fd173e728060ce0d4d9197416ce78e77fd1
PeDro0210 <pedroruben021@gmail.com>
2025-09-13 20:15:57 -0600

    chore: reformulating parsing nfa

--- src/parsing/nfa.py
+++ src/parsing/nfa.py
@@ -25,9 +25,8 @@ class ThompsonsNFA:
 def postfixToNfa(postfix: str) -> ThompsonsNFA:
     nfaStack:list[ThompsonsNFA] = []
 
-    for c_idx in range(len(postfix)):
-        
-        match postfix[c_idx]:
+    for c in postfix:
+        match c:
             case '∗':
 
                 # We take the ThompsonsNFA that we want to apply the clean
@@ -75,23 +74,7 @@ def postfixToNfa(postfix: str) -> ThompsonsNFA:
 
                 nfaStack.append(ThompsonsNFA(initial, accept))
 
-            case "/":
-                # we ignore the '/' cause in case it before another value,
-
-                # As Pablo told me, we create the symetrical nodes
-                initial: State = State() # the initial of the actual ThompsonsNFA we are doing, not the whole construct
-                accept: State = State()
-
-                initial.label, initial.edge1 = c, accept #type: ignore
-
-                # Goes to the stack for any other operators
-                nfaStack.append(ThompsonsNFA(initial, accept))
-
-
-                continue
             case _:
-
-                c = postfix[c_idx]
 
                 if c == '𝜀':
                     c = None # when traversing, it will be better just to check for a 'None' type for taking advantage of the syntatic sugar
