47732daadf4b8610a9b70a378c968f71e2bb71cd
PeDro0210 <pedroruben021@gmail.com>
2025-09-13 19:54:50 -0600

    chore: number equivalnce-class implementation

--- src/utils/character_parsing.py
+++ src/utils/character_parsing.py
@@ -4,7 +4,10 @@ import re
 
 
 eq_classes:dict[str,str] = {
-    "a-z":"a|b|c|d|e|f|g|h|i|j|k|l|m|o|p|q|r|s|t|u|v|w|x|y|z"
+    "a-z":"a|b|c|d|e|f|g|h|i|j|k|l|m|o|p|q|r|s|t|u|v|w|x|y|z",
+    "A-Z":"A|B|C|D|E|F|G|H|I|J|K|L|M|O|P|Q|R|S|T|U|V|W|X|Y|Z",
+    "0-9":"0|1|2|3|4|5|6|7|8|9"
+
 }
 
 # given a equivalence class, it returns the shunting yard's friendly string
